82d3ae117e812d6acf21569850554e8cd20e7f94
YasunariOsakabe <yasunari0296@gmail.com>
2023-02-06 22:20:32 +0900

    CallBackを使った計算処理を追記

--- CallBackProject/ViewController.swift
+++ CallBackProject/ViewController.swift
@@ -11,27 +11,40 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func tappedButton(_ sender: Any) {
-        caluculateNumber {
-            print("処理3: ボタンがタップされました。")
+        //値を受け取る
+        caluculateNumber { result in
+            let sum = result / 2
+            print("処理3: ボタンがタップされました。計算結果は\(sum)です")
+
         }
+        
+//        caluculateNumber {
+//            print("処理3: ボタンがタップされました。")
+//        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        caluculateNumber {
-            print("処理2: 処理1実行後に実行されます")
+        //値を受け取る
+        caluculateNumber { result in
+            let sum = result / 4
+            print("処理2: 処理1実行後に実行されます。計算結果は\(sum)です")
+
         }
+//        caluculateNumber {
+//            print("処理2: 処理1実行後に実行されます")
+//        }
     }
     
     
-    private func caluculateNumber(completion: () -> Void) {
-        let num1 = 2
+    private func caluculateNumber(completion: (Int) -> Void) {
+        let num1 = 3
         let num2 = 5
         
         let result1 = num1 + num2
         
         print("処理1: \(result1)")
-        completion()  //ここで処理2,3へ飛ぶ
+        completion(result1)  //ここで処理2,3へ飛ぶ
     }
 
 }
